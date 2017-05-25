958f5bf8fd5d16c22f282a96d54ad1f8ddae5240
Moritz Angermann <moritz.angermann@gmail.com>
2017-05-25 16:34:40 +0800

    Explicit File IO

--- src/Development/GitRev.hs
+++ src/Development/GitRev.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE CPP        #-}
 
 -- |
 -- Module      :  $Header$
@@ -46,52 +47,82 @@ import Control.Exception
 import Control.Monad
 import Data.Maybe
 import Language.Haskell.TH
-import Language.Haskell.TH.Syntax
-import System.Directory
+import Language.Haskell.TH.Syntax (addDependentFile, trueName, falseName)
 import System.Exit
 import System.FilePath
-import System.Process
 
 import Prelude ()
-import Prelude.Compat
+import Prelude.Compat hiding (readFile)
+
+#if MIN_VERSION_template_haskell(2,13,0)
+import qualified Language.Haskell.TH.Syntax as TH
+#else
+import qualified Process as Proc
+import qualified Directory as Dir
+import qualified Prelude.Compat as P
+#endif
+
+readFile :: FilePath -> Q String
+doesFileExist :: FilePath -> Q Bool
+doesDirectoryExist :: FilePath -> Q Bool
+getCurrentDirectory :: Q FilePath
+findExecutables :: String -> Q [FilePath]
+readProcessWithExitCode :: FilePath -> [String] -> String
+                        -> Q (ExitCode, String, String)
+
+#if MIN_VERSION_template_haskell(2,13,0)
+readFile = TH.readFile
+doesFileExist = TH.doesFileExist
+doesDirectoryExist = TH.doesDirectoryExist
+getCurrentDirectory = TH.getCurrentDirectory
+findExecutables = TH.findExecutables
+readProcessWithExitCode p args = TH.readProcessWithExitCode p args
+#else
+readFile = runIO . P.readFile
+doesFileExist = runIO . Dir.doesFileExist
+doesDirectoryExist = runIO . Dir.doesDirectoryExist
+getCurrentDirectory = runIO Dir.getCurrentDirectory
+findExecutables = runIO . Proc.findExecutables
+readProcessWithExitCode p args = runIO . Proc.readProcessWithExitCode p args
+#endif
 
 -- | Run git with the given arguments and no stdin, returning the
 -- stdout output. If git isn't available or something goes wrong,
 -- return the second argument.
 runGit :: [String] -> String -> IndexUsed -> Q String
 runGit args def useIdx = do
-  let oops :: SomeException -> IO (ExitCode, String, String)
-      oops _e = return (ExitFailure 1, def, "")
-  gitFound <- runIO $ isJust <$> findExecutable "git"
+--  let oops :: SomeException -> IO (ExitCode, String, String)
+--      oops _e = return (ExitFailure 1, def, "")
+  gitFound <- isJust . listToMaybe <$> findExecutables "git"
   if gitFound
     then do
       -- a lot of bookkeeping to record the right dependencies
-      pwd <- runIO getDotGit
+      pwd <- getDotGit
       let hd         = pwd </> ".git" </> "HEAD"
           index      = pwd </> ".git" </> "index"
           packedRefs = pwd </> ".git" </> "packed-refs"
-      hdExists  <- runIO $ doesFileExist hd
+      hdExists  <- doesFileExist hd
       when hdExists $ do
         -- the HEAD file either contains the hash of a detached head
         -- or a pointer to the file that contains the hash of the head
-        splitAt 5 `fmap` runIO (readFile hd) >>= \case
+        splitAt 5 `fmap` (readFile hd) >>= \case
           -- pointer to ref
           ("ref: ", relRef) -> do
             let ref = pwd </> ".git" </> relRef
-            refExists <- runIO $ doesFileExist ref
+            refExists <- doesFileExist ref
             when refExists $ addDependentFile ref
           -- detached head
           _hash -> addDependentFile hd
       -- add the index if it exists to set the dirty flag
-      indexExists <- runIO $ doesFileExist index
+      indexExists <- doesFileExist index
       when (indexExists && useIdx == IdxUsed) $ addDependentFile index
       -- if the refs have been packed, the info we're looking for
       -- might be in that file rather than the one-file-per-ref case
       -- handled above
-      packedExists <- runIO $ doesFileExist packedRefs
+      packedExists <- doesFileExist packedRefs
       when packedExists $ addDependentFile packedRefs
-      runIO $ do
-        (code, out, _err) <- readProcessWithExitCode "git" args "" `catch` oops
+      do
+        (code, out, _err) <- readProcessWithExitCode "git" args "" -- `catch` oops
         case code of
           ExitSuccess   -> return (takeWhile (/= '\n') out)
           ExitFailure _ -> return def
@@ -99,7 +130,7 @@ runGit args def useIdx = do
 
 -- | Determine where our @.git@ directory is, in case we're in a
 -- submodule.
-getDotGit :: IO FilePath
+getDotGit :: Q FilePath
 getDotGit = do
   pwd <- getGitRoot
   let dotGit = pwd </> ".git"
@@ -118,7 +149,7 @@ getDotGit = do
            _ -> oops
 
 -- | Get the root directory of the Git repo.
-getGitRoot :: IO FilePath
+getGitRoot :: Q FilePath
 getGitRoot = do
   pwd <- getCurrentDirectory
   (code, out, _) <-
